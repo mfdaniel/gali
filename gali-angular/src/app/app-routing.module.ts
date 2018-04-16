360422211e940b8c2912a94532346175af37761d
GabrielAbrDam <gabriel.abreudamaso@gmail.com>
2018-04-16 12:34:06 +0200

    create app.routing.module.ts

---
+++ gali-angular/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+// import  add home component
+// import add aboutUs component
+
+const routes: Routes = [
+
+    // place for the paths; object {}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
